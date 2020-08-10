1ed2f79f1e480e28383ac360b85f9ba6681de224
unknown <jusakirawadi@gmail.com>
2020-08-10 21:50:50 +0700

    Laravel CRUD (dengan Eloquent ORM)

--- app/Http/Controllers/PertanyaanController.php
+++ app/Http/Controllers/PertanyaanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Pertanyaan;
 
 class PertanyaanController extends Controller
 {
@@ -18,27 +19,45 @@ class PertanyaanController extends Controller
             'isi' => 'required',
         ]);
     
-        $query = DB::table('pertanyaan')->insert([
-                'judul' => $req['judul'], 
-                 'isi' => $req['isi'],
-                 'profil_id' => 1
-                 ]);
+        //$query = DB::table('pertanyaan')->insert([
+        //        'judul' => $req['judul'], 
+        //         'isi' => $req['isi'],
+        //         'profil_id' => 1
+        //         ]);
         
+        //$list = new Pertanyaan;
+        //$list->judul = $req['judul'];
+        //$list->isi = $req['isi'];
+        //$list->save(); 
+
+        $list = Pertanyaan::create([
+            'judul' => $req['judul'],
+            'isi' => $req['isi']
+        ]);
+
         return redirect('/pertanyaan')->with('sukses','Pertanyaan berhasil dibuat !');
     }
     
     public function index () {
-        $list = DB::table('pertanyaan')->get();        
+        //$list = DB::table('pertanyaan')->get();        
+        $list = Pertanyaan::all();
         return view('pertanyaan.index',compact('list'));
     }
 
     public function show ($id) {
-        $list = DB::table('pertanyaan')->where('id',$id)->first();        
+        //$list = DB::table('pertanyaan')->where('id',$id)->first();        
+
+        //$list = Pertanyaan::find($id);
+        $list = Pertanyaan::where('id', $id)->first();
+
         return view('pertanyaan.show',compact('list'));
     }
 
     public function edit ($id) {
-        $list = DB::table('pertanyaan')->where('id',$id)->first();        
+        //$list = DB::table('pertanyaan')->where('id',$id)->first();    
+        
+        $list = Pertanyaan::find($id);
+
         return view('pertanyaan.edit',compact('list'));
     }
 
@@ -49,19 +68,35 @@ class PertanyaanController extends Controller
             'isi' => 'required',
         ]);
 
-        $list = DB::table('pertanyaan')
-                    ->where('id',$id)
-                    ->update([
-                        'judul' => $req['judul'],
-                        'isi' => $req['isi']
-                    ]);        
+        //$list = DB::table('pertanyaan')
+        //            ->where('id',$id)
+        //            ->update([
+        //                'judul' => $req['judul'],
+        //                'isi' => $req['isi']
+        //            ]);        
+
+        //$list = Pertanyaan::find($id);
+        //$list->judul = $req['judul'];
+        //$list->isi = $req['isi'];
+        //$list->save();
+
+        $list = Pertanyaan::where('id',$id)->update([
+                            'judul' => $req['judul'],
+                            'isi' => $req['isi']
+        ]);
 
         return redirect('/pertanyaan')->with('sukses','Pertanyaan berhasil diupdate !');
     }
 
     public function destroy($id) {
 
-        $list = DB::table('pertanyaan')->where('id',$id)->delete();
+        //$list = DB::table('pertanyaan')->where('id',$id)->delete();
+
+        //$list = Pertanyaan::find($id);
+        //$list->delete();
+
+        $list = Pertanyaan::where('id',$id)->delete();
+
         return redirect('/pertanyaan')->with('sukses','Pertanyaan berhasil dihapus !');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/pertanyaan', 'PertanyaanController@index');
-Route::get('/pertanyaan/create', 'PertanyaanController@create');
-Route::post('/pertanyaan','PertanyaanController@store');
-Route::get('/pertanyaan/{pertanyaan_id}','PertanyaanController@show');
-Route::get('/pertanyaan/{pertanyaan_id}/edit','PertanyaanController@edit');
-Route::put('/pertanyaan/{pertanyaan_id}','PertanyaanController@update');
-Route::delete('/pertanyaan/{pertanyaan_id}','PertanyaanController@destroy');
+//Route::get('/pertanyaan', 'PertanyaanController@index');
+//Route::get('/pertanyaan/create', 'PertanyaanController@create');
+//Route::post('/pertanyaan','PertanyaanController@store');
+//Route::get('/pertanyaan/{pertanyaan_id}','PertanyaanController@show');
+//Route::get('/pertanyaan/{pertanyaan_id}/edit','PertanyaanController@edit');
+//Route::put('/pertanyaan/{pertanyaan_id}','PertanyaanController@update');
+//Route::delete('/pertanyaan/{pertanyaan_id}','PertanyaanController@destroy');
+
+Route::resource('pertanyaan','PertanyaanController');
